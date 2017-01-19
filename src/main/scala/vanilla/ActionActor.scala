c151885800b22c8718c0c7e424fa0da44c98ffb7
Remco <remcowesterhoud@msn.com>
2017-01-19 11:05:37 +0100

    Update ActionActor.scala

--- src/main/scala/vanilla/ActionActor.scala
+++ src/main/scala/vanilla/ActionActor.scala
@@ -7,9 +7,11 @@ import shared.Timer
  * Created by RemcoW on 16-11-2016.
  */
 class ActionActor extends Actor {
+  // When the ActionActor receives a message containing a Timer it will call the performAction() method on it
   override def receive = {
     case x: Timer =>
       x.performAction()
+    // The Timer get send to an Actor in this ActorSystem with the address /user/out
       context.actorSelection("/user/out") ! x
     case _ =>
       println("default")
