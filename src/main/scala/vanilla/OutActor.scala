56e04bc3bca1c43ca3ddca6db37ebddfb5019167
Remco <remcowesterhoud@msn.com>
2017-01-19 11:06:07 +0100

    Update OutActor.scala

--- src/main/scala/vanilla/OutActor.scala
+++ src/main/scala/vanilla/OutActor.scala
@@ -7,6 +7,7 @@ import shared.Timer
  * Created by RemcoW on 16-11-2016.
  */
 class OutActor extends Actor {
+  // When the "out" Actor receives a message containing a Timer it will call the stopClock() method on this Timer
   override def receive = {
     case x: Timer =>
       x.stopClock()
